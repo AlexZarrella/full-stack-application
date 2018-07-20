298cb3f098f470f9c8d88a16bcf240549b2747e2
alexzarrella <alexzarrella@Alexs-MacBook-Pro-3.local>
2018-07-20 11:23:37 -0400

    more seeds

--- bin/seeds.js
+++ bin/seeds.js
@@ -299,6 +299,182 @@ const shows = [{
     image: "/images/showpic/bigmouthcover.jpg",
     cast: ["Nick Kroll", "John Mulaney", "Fred Armisen"],
     summary: "Teenage friends find their lives upended by the wonders and horrors of puberty."
+},
+{
+    title: "Jane the Virgin",
+    genre: "Comedy",
+    image: "/images/showpic/janethevirgincover.jpg",
+    cast: ["Gina Rodriguez", "Andrea Navedo", "Yael Grobglas"],
+    summary: "A young, devout Catholic woman discovers that she was accidentally artificially inseminated."
+},
+{
+    title: "Riverdale",
+    genre: "Mystery",
+    image: "/images/showpic/riverdalecover.jpg",
+    cast: ["K.J. Apa", "Lili Reinhart", "Camila Mendes"],
+    summary: "A subversive take on Archie and his friends, exploring small town life, the darkness and weirdness bubbling beneath Riverdale's wholesome facade."
+
+},
+{
+    title: "Charmed",
+    genre: "Drama",
+    image: "/images/showpic/charmedcover.jpg",
+    cast: ["Holly Marie Combs", "Alyssa Milano", "Brian Krause"],
+    summary: "Three sisters discover their destiny, to battle against the forces of evil, using their witchcraft. They are the Charmed Ones."
+},
+{
+    title: "Portlandia",
+    genre: "Comedy",
+    image: "/images/showpic/portlandiacover.jpg",
+    cast: ["Fred Armisen", "Carrie Brownstein", "Kyle MacLachlan"],
+    summary: "A sketch-comedy series that parodies life in Portland, Oregon."
+},
+{
+    title: "Containment",
+    genre: "Drama",
+    image: "/images/showpic/containmentcover.jpg",
+    cast: ["David Gyasi", "Christina Marie Moses", "Chris Wood"],
+    summary: "The series follows an epidemic that breaks out in Atlanta, leaving the large city quarantined and those stuck on the inside fighting for their lives."
+},
+{
+    title: "Mad Men",
+    genre: "Drama",
+    image: "/images/showpic/madmencover.jpg",
+    cast: ["Jon Hamm", "Elisabeth Moss", "Vincent Kartheiser"],
+    summary: "A drama about one of New York's most prestigious ad agencies at the beginning of the 1960s, focusing on one of the firm's most mysterious but extremely talented ad executives, Donald Draper."
+},
+{
+    title: "New Girl",
+    genre: "Comedy",
+    image: "/images/showpic/newgirlcover.jpg",
+    cast: ["Zooey Deschanel", "Jake Johnson", "Max Greenfield"],
+    summary: "After a bad break-up, Jess, an offbeat young woman, moves into an apartment loft with three single men. Although they find her behavior very unusual, the men support her - most of the time."
+},
+{
+    title: "The Rain",
+    genre: "Sci-Fi",
+    image: "/images/showpic/theraincover.jpg",
+    cast: ["Alba August", "Lynggaard Tonnesen", "Mikken Boe Folsgaard"],
+    summary: "After a brutal virus wipes out most of the population, two young siblings embark on a perilous search for safety."
+},
+{
+    title: "Unbreakable: Kimmy Schmidt",
+    genre: "Comedy",
+    image: "/images/showpic/unbreakablecover.jpg",
+    cast: ["Ellie Kemper", "Jane Krakowski", "Tituss Burgess"],
+    summary: "A woman is rescued from a doomsday cult and starts life over again in New York City."
+},
+{
+    title: "House of Cards",
+    genre: "Drama",
+    image: "/images/showpic/houseofcardscover.jpg",
+    cast: ["Kevin Spacey", "Michel Gill", "Robin Wright"],
+    summary: "A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him."
+},
+{
+    title: "Luther",
+    genre: "Crime",
+    image: "/images/showpic/luthercover.jpg",
+    cast: ["Idris Elba", "Dermot Crowley", "Michael Smiley"],
+    summary: "DCI John Luther is a near-genius murder detective whose brilliant mind can't always save him from the dangerous violence of his passions."
+},
+{
+    title: "The Keepers",
+    genre: "Documentary",
+    image: "/images/showpic/thekeepers.jpg",
+    cast: ["Gemma Hoskins", "Abbie Schnaub", "Jean Hargadon Wehner"],
+    summary: "A seven-part docuseries about the unsolved murder of a nun and the horrific secrets and pain that linger nearly five decades after her death."
+},
+{
+    title: "Grey's Anatomy",
+    genre: "Drama",
+    image: "/images/showpic/greysanatomycover.jpg",
+    cast: ["Ellen Pompeo", "Justin Chambers", "Chandra Wilson"],
+    summary: "A drama centered on the personal and professional lives of five surgical interns and their supervisors."
+},
+{
+    title: "Fuller House",
+    genre: "Comedy",
+    image: "/images/showpic/fullerhousecover.jpg",
+    cast: ["Candace Cameron Bure", "Jodi Sweetin", "Andrea Barber"],
+    summary: "D.J. Fuller is a mother of three young boys and is a recent widow. D.J.'s sister Stephanie, her best friend Kimmy and Kimmy's teenage daughter all move in to help raise her sons. The house is now a lot fuller."
+},
+{
+    title: "Bunk'd",
+    genre: "Comedy",
+    image: "/images/showpic/bunkdcover.jpg",
+    cast: ["Peyton List", "Karan Brar", "Skai Jackson"],
+    summary: "Emma, Ravi, and Zuri Ross head off to a rustic summer camp in Maine, where their parents met as teens."
+},
+{
+    title: "Supernatural",
+    genre: "Drama",
+    image: "/images/showpic/supernaturalcover.jpg",
+    cast: ["Jared Padalecki", "Jensen Ackles", "Jim Beaver"],
+    summary: "Two brothers follow their father's footsteps as hunters, fighting evil supernatural beings of many kinds, including monsters, demons, and gods that roam the earth."
+},
+{
+    title: "NCIS",
+    genre: "Crime",
+    image: "/images/showpic/nciscover.jpg",
+    cast: ["Mark Harmon", "David McCallum", "Pauley Perrette"],
+    summary: "The cases of the Naval Criminal Investigative Service's Washington DC Major Case Response Team, led by Special Agent Leroy Jethro Gibbs."
+},
+{
+    title: "Arrow",
+    genre: "Action",
+    image: "/images/showpic/arrowcover.jpg",
+    cast: ["Stephen Amell", "Katie Cassidy", "David Ramsey"],
+    summary: "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow."
+},
+{
+    title: "Pokemon: Sun & Moon",
+    genre: "Anime",
+    image: "/images/showpic/pokemoncover.jpg",
+    cast: ["Anthony Fernandez", "Miguel Fernandez", "Zee Andrews"],
+    summary: "Ash is new to the Alola region. He's found some new friends, and new Pokémon too."
+},
+{
+    title: "One Punch Man",
+    genre: "Anime",
+    image: "/images/showpic/onepunchmancover.jpg",
+    cast: ["Makoto Furukawa", "Max Mittelman", "Kaito Ishikawa"],
+    summary: "The story of Saitama, a hero that does it just for fun & can defeat his enemies with a single punch."
+},
+{
+    title: "Fullmetal Alchemist",
+    genre: "Anime",
+    image: "/images/showpic/fullmetalcover.jpg",
+    cast: ["Iemasa Kayumi", "Kent Williams", "Muriel Hofmann"],
+    summary: "Two brothers search for a Philosopher's Stone after an attempt to revive their deceased mother goes awry and leaves them in damaged physical forms."
+},
+{
+    title: "Fauda",
+    genre: "Action",
+    image: "/images/showpic/faudacover.jpg",
+    cast: ["Lior Raz", "Shadi Mar'i", "Laetitia Eido"],
+    summary: "Fauda (Arabic for Chaos) depicts the two-sided story of the Israeli-Palestinian conflict. "
+},
+{
+    title: "The Ranch",
+    genre: "Comedy",
+    image: "/images/showpic/ranchcover.jpg",
+    cast: ["Ashton Kutcher", "Debra Winger", "Sam Elliot"],
+    summary: "The son of a Colorado rancher returns home from a semi-pro football career to run the family business."
+},
+{
+    title: "Pretty Little Liars",
+    genre: "Drama",
+    image: "/images/showpic/prettylittleliarscover.jpg",
+    cast: ["Troian Bellisario", "Ashley Benson", "Lucy Hale"],
+    summary: "Four friends band together against an anonymous foe who threatens to reveal their darkest secrets, while also investigating the disappearance of their best friend."
+},
+{
+    title: "Sacred Games",
+    genre: "Action",
+    image: "/images/showpic/sacredgamescover.jpg",
+    cast: ["Saif Ali Khan", "Nawazuddin Siddiqui", "Radhika Apte"],
+    summary: "A link in their pasts leads an honest cop to a fugitive gang boss, whose cryptic warning spurs the officer on a quest to save Mumbai from cataclysm."
 }]
 
 Show.create(shows)
@@ -589,8 +765,126 @@ const movies = [
     image: "/images/moviepic/forgottencover.jpg",
     cast: ["Ha-Neul Kang", "Mu-Yeol Kim", "Seong-kun Mun"],
     plot: "Jin-Seok (Kang Ha-Neul) moves into a new home with his older brother Yoo-Seok (Kim Moo-Yul), mother (Na Young-Hee) and father (Moon Sung-Geun). Jin-Seok suffers from hypersensitivity, but with medication he is able to live normally."
-}
-]
+},
+{
+    title: "The Ritual",
+    genre: "Horror",
+    image: "/images/moviepic/ritualcover.jpg",
+    cast: ["Rafe Spall", "Arsher Ali", "Robert James-Collier"],
+    plot: "A group of college friends reunite for a trip to the forest, but encounter a menacing presence in the woods that's stalking them."
+},
+{
+    title: "The Conjuring",
+    genre: "Horror",
+    image: "/images/moviepic/conjuringcover.jpg",
+    cast: ["Patrick Wilson", "Vera Farmiga", "Ron Livingston"],
+    plot: "Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence in their farmhouse."
+},
+{
+    title: "Meet the Blacks",
+    genre: "Comedy",
+    image: "/images/moviepic/meettheblackscover.jpg",
+    cast: ["Mike Epps", "Zulay Henao", "Alex Henderson"],
+    plot: "As Carl Black gets the opportunity to move his family out of Chicago in hope of a better life, their arrival in Beverly Hills is timed with that city's annual purge, where all crime is legal for twelve hours."
+},
+{
+    title: "Bee Movie",
+    genre: "Animation",
+    image: "/images/moviepic/beemoviecover.jpg",
+    cast: ["Jerry Seinfeld", "Renee Zellweger", "Matthew Broderick"],
+    plot: "Barry B. Benson, a bee just graduated from college, is disillusioned at his lone career choice: making honey. On a special trip outside the hive, Barry's life is saved by Vanessa, a florist in New York City. "
+},
+{
+    title: "Wind River",
+    genre: "Crime",
+    image: "/images/moviepic/windrivercover.jpg",
+    cast: ["Kelsey Asbille", "Jeremy Renner", "Julia Jones"],
+    plot: "A veteran hunter helps an FBI agent to investigate the murder of a young woman on a Wyoming Native American reservation."
+},
+{
+    title: "Madagascar 2",
+    genre: "Animation",
+    image: "/images/moviepic/madagascar2.jpg",
+    cast: ["Ben Stiller", "Chris Rock", "David Schwimmer"],
+    plot: "The animals try to fly back to New York City, but crash-land on an African wildlife refuge, where Alex is reunited with his parents."
+},
+{
+    title: "Gone Baby Gone",
+    genre: "Crime",
+    image: "/images/moviepic/gonebabygonecover.jpg",
+    cast: ["Morgan Freeman", "Ed Harris", "Casey Affleck"],
+    plot: "Two Boston area detectives investigate a little girl's kidnapping, which ultimately turns into a crisis both professionally and personally."
+},
+{
+    title: "Duck Duck Goose",
+    genre: "Animation", 
+    image: "/images/moviepic/duckduckgoosecover.jpg",
+    cast: ["Jim Gaffigan", "Zendaya", "Lance Lim"],
+    plot: "A bachelor goose must form a bond with two lost ducklings as they journey south."
+},
+{
+    title: "The Cloverfield Paradox",
+    genre: "Horror",
+    image: "/images/moviepic/cloverfieldcover.jpg",
+    cast: ["Gugu Mbatha-Raw", "David Oyelowo", "Daniel Bruhl"],
+    plot: "Orbiting a planet on the brink of war, scientists test a device to solve an energy crisis, and end up face-to-face with a dark alternate reality."
+},
+{
+    title: "Scooby-Doo",
+    genre: "Adventure",
+    image: "/images/moviepic/scoobydoo.jpg",
+    cast: ["Matthew Lillard", "Freddie Prinze Jr.", "Sarah Michelle Gellar"],
+    plot: "After an acrimonious break up, the Mystery Inc. gang are individually brought to an island resort to investigate strange goings on."
+},
+{
+    title: "Spanglish",
+    genre: "Comedy",
+    image: "/images/moviepic/spanglishcover.jpg",
+    cast: ["Adam Sandler", "Tea Leoni", "Paz Vega"],
+    plot: "A woman and her daughter emigrate from Mexico for a better life in America, where they start working for a family where the patriarch is a newly celebrated chef with an insecure wife."
+},
+{
+    title: "Moana",
+    genre: "Animation",
+    image: "/images/moviepic/moanacover.jpg",
+    cast: ["Auli'i Cravalho", "Dwayne Johnson", "Rachel House"],
+    plot: "In Ancient Polynesia, when a terrible curse incurred by the Demigod Maui reaches Moana's island, she answers the Ocean's call to seek out the Demigod to set things right."
+},
+{
+    title: "Bad Boys",
+    genre: "Action",
+    image: "/images/moviepic/badboyscover.jpg",
+    cast: ["Will Smith", "Martin Lawrence", "Lisa Boyle"],
+    plot: "Two hip detectives protect a witness to a murder while investigating a case of stolen heroin from the evidence storage room from their police precinct."
+},
+{
+    title: "Ghostbusters",
+    genre: "Action",
+    image: "/images/moviepic/ghostbusterscover.jpg",
+    cast: ["Bill Murray", "Dan Aykroyd", "Sigourney Weaver"],
+    plot: "Three former parapsychology professors set up shop as a unique ghost removal service."
+},
+{
+    title: "The Sixth Sense",
+    genre: "Mystery",
+    image: "/images/moviepic/sixthsensecover.jpg",
+    cast: ["Bruce Willis", "Haley Joel Osment", "Toni Collette"],
+    plot: "A boy who communicates with spirits seeks the help of a disheartened child psychologist."
+},
+{
+    title: "Lincoln",
+    genre: "History",
+    image: "/images/moviepic/lincolncover.jpg",
+    cast: ["Daniel Day-Lewis", "Sally Field", "David Strathairn"],
+    plot: "As the American Civil War continues to rage, America's president struggles with continuing carnage on the battlefield as he fights with many inside his own cabinet on the decision to emancipate the slaves."
+},
+{
+    title: "The King's Speech",
+    genre: "History",
+    image: "/images/moviepic/kingsspeechcover.jpg",
+    cast: ["Colin Firth", "Geoffrey Rush", "Helena Bonham Carter"],
+    plot: "The story of King George VI of the United Kingdom of Great Britain and Northern Ireland, his impromptu ascension to the throne and the speech therapist who helped the unsure monarch become worthy of it."
+}]
 
 
 Movie.create(movies)
